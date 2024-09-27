4eafc52a4a0c6c6e9976c995750f7fa6eb1dcce8
mahesh2121 <maheshgaikwad128@gmail.com>
2024-09-27 17:09:12 +0530

    ec2-ssh-using-create-keys

--- main.tf
+++ main.tf
@@ -9,13 +9,21 @@ terraform {
 resource "aws_instance" "Pipeline-machine" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name = "id_rsa"
 
-  network_interface {
-    network_interface_id = var.network_interface_id
-    device_index         = 0
-  }
+  # network_interface {
+  #   network_interface_id = var.network_interface_id
+  #   device_index         = 0
+  # }
 
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
+  # credit_specification {
+  #   cpu_credits = "unlimited"
+  # }
+}
+
+resource "aws_key_pair" "mahesh" {
+  key_name = "id_rsa"
+  #Created public key need copy here in public_key parameter and using private key you can acccess ec2 severs
+  public_key = "ssh-rsa"
+  
 }
